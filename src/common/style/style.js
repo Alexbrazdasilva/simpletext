60e303e26186742a6f39be26236bffc3abfbec3d
alexbrazreal <alexbrazdasilva@gmail.com>
2021-07-11 00:29:27 -0300

    Configurar tema e cores do vuetify

---
+++ src/common/style/style.js
@@ -0,0 +1,13 @@
+import colors from "vuetify/lib/util/colors";
+const { orange, grey } = colors;
+
+const themes = {
+  light: {
+    primary: orange,
+  },
+  dark: {
+    primary: grey.darken4,
+  },
+};
+
+export default themes;
