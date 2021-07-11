d7aa1380768aefae283aa5a45d92f456daea54c7
alexbrazreal <alexbrazdasilva@gmail.com>
2021-07-11 00:27:47 -0300

    Revert "Revert "Adicionar alias de middlewares""
    This reverts commit 1917ee75df68178a537c472cd2ffbda51b9b330f.

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,7 @@ module.exports = {
         "@components": path.resolve(__dirname, "src", "components"),
         "@views": path.resolve(__dirname, "src", "views"),
         "@common": path.resolve(__dirname, "src", "common"),
+        "@middlewares": path.resolve(__dirname, "src", "middlewares"),
       },
     },
   },
