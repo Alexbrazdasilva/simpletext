6e882676d9287e55a04b60daa9e78a3d1a302281
alexbrazreal <alexbrazdasilva@gmail.com>
2021-07-20 20:39:24 -0300

    Adicionar módulo do usuário

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,14 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import createPersistedState from 'vuex-persistedstate'
+import createPersistedState from "vuex-persistedstate";
+
+import user from "./modules/user";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
-  plugins: [createPersistedState()]
+  modules: {
+    user,
+  },
+  plugins: [createPersistedState()],
 });
