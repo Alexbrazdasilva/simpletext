e2973773135ddfd9fdd5e9c4724f336e17f3b862
alexbrazreal <alexbrazdasilva@gmail.com>
2021-07-20 20:40:06 -0300

    Implementar validação de mínimo de caracteres e v-model

--- src/common/components/FormRegister.vue
+++ src/common/components/FormRegister.vue
@@ -3,13 +3,13 @@
     <text-field
       label="Nome"
       v-model="name"
-      :rules="[validity.required, validity.max]"
+      :rules="[validity.required, validity.min]"
       :properties="{ outlined: true }"
-      @input="validityMax"
+      :maxlength="10"
     ></text-field>
     <text-field
-      v-model="form.name"
       label="Username"
+      v-model="username"
       :readonly="true"
       hint="Gerado a partir do nome inserido no primeiro campo"
       :properties="{ outlined: true, disabled: true, persistentHint: true }"
@@ -17,13 +17,13 @@
     ></text-field>
     <div class="d-flex">
       <v-spacer></v-spacer>
-      <v-btn color="primary" @click="registerUser">Entrar</v-btn>
+      <v-btn color="primary" @click="submitForm">Entrar</v-btn>
     </div>
   </form>
 </template>
 
 <script>
-import { isEmpty } from "@common/utils/validityUser.js";
+import { isEmpty, isMin } from "@common/utils/validityUser.js";
 import TextField from "@components/TextField.vue";
 export default {
   data: () => ({
@@ -31,25 +31,18 @@ export default {
     username: "Meunome",
     validity: {
       required: (value) => isEmpty(value),
-      max: (value) => value.length <= 10 || "Preencha com um nome mais curto",
+      min: (value) => isMin(value, 2),
     },
   }),
   methods: {
     onfocus(e) {
       e.preventDefault();
     },
-    registerUser() {
-      //
+    submitForm() {
+      const payload = { name: this.name, username: this.username };
+      this.$emit("submit", payload);
     },
-    validityMax({ value, preventDefault }) {
-      value.lenght == 10 && preventDefault();
-    },
-    convertName(name) {
-      let separedName = name.replace(/\s/g, "").normalize("NFC").toLowerCase();
-      console.clear();
-      console.log(separedName);
-      return name;
-    },
+    convertName: (name) => name.replace(/\s/g, ""),
   },
   watch: {
     name(value) {
